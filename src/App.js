4d0bc9efed3f9bb5ed1b57e0ebc12714b9971bd9
jewelran <mdjewelrana6487@gmail.com>
2021-03-16 00:27:54 +0600

    recap complate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [randomUsers, setUsers] = useState([])
+  useEffect(() =>{
+    fetch("https://randomuser.me/api?results=10")
+    .then(res => res.json())
+    .then(data => setUsers(data.results))
+  },[])
+  console.log(randomUsers);
+
+  const hideDisplay = () =>{
+    const hide = document.getElementById("userContainer");
+    hide.style.display="none"
+    console.log('tur call me');
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>total user: {randomUsers.length}</h1>
+
+        {
+          randomUsers.map(user => <Users userInfo = {user} key = {user.cell}></Users>)
+        }
+        
       </header>
     </div>
   );
 }
+function Users (props){
 
+  const userStyel = {
+    border:"1px solid red",
+    padding: "10px",
+    margin: "10px",
+    borderRadius: "10px",
+    width: "450px",
+    height: "300px",
+    float:'left',
+    lineHeight:'15px'
+  }
+  return(
+    <div  style = {userStyel}>
+     <h2>{props.userInfo.name.title} {props.userInfo.name.fast} {props.userInfo.name.last}</h2>
+       <h4>Email: {props.userInfo.email}</h4>
+       <h4>Phone: {props.userInfo.phone}</h4>
+      <img style = {{borderRadius: "50%"}} src={props.userInfo.picture.large} alt=""/>
+    </div>
+  )
+}
 export default App;
